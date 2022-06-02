28e169fea1db0f4994b29f14a42b99055de1a200
Jacob Krogman <jake@wlabel.co>
2022-06-02 14:05:45 -0400

    this should go here (proc-macro restrictions)

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,15 @@ pub trait StateObject {
   fn load() -> Self;
   fn save(&self) -> Cid;
 }
+
+/// A macro to abort concisely.
+/// This should be part of the SDK as it's very handy.
+#[macro_export]
+macro_rules! abort {
+  ($code:ident, $msg:literal $(, $ex:expr)*) => {
+      fvm_sdk::vm::abort(
+          fvm_shared::error::ExitCode::$code.value(),
+          Some(format!($msg, $($ex,)*).as_str()),
+      )
+  };
+}
