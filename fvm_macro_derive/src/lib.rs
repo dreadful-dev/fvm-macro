186b4b97808747908781f2fb8f0287efcc5d89b6
Jacob Krogman <jake@wlabel.co>
2022-06-02 14:03:41 -0400

    export macro

--- fvm_macro_derive/src/lib.rs
+++ fvm_macro_derive/src/lib.rs
@@ -15,6 +15,7 @@ struct FvmActorMacroAttributes {
 
 /// A macro to abort concisely.
 /// This should be part of the SDK as it's very handy.
+#[macro_export]
 macro_rules! abort {
   ($code:ident, $msg:literal $(, $ex:expr)*) => {
       fvm_sdk::vm::abort(
